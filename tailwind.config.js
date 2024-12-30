f5928d086b639649c9be6c706be95f2e7f5c42cd
unawarexi <unawarexi@gmail.com>
2024-12-30 15:44:14 +0100

    updated chmod +x for vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      
-    },
+    extend: {},
   },
   plugins: [],
 }
