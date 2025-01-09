3ef852f2b021a3ca851108266cd47129328f1ee3
unawarexi <unawarexi@gmail.com>
2025-01-09 16:40:10 +0100

    about page fixed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,23 +21,23 @@ const About = () => {
 </div>
 
         {/* About Me Content */} 
-        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-left">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16  flex flex-col md:items-start md:text-left items-center text-left">
+          <h1 className="title-font sm:text-4xl text-xl mb-4 font-medium text-gray-900 -ml-20 md:-ml-0">
             <span className='text-blue-300 text-3xl'>Welcome,</span> <br />I'm Andrew Chukwuweike
             <br className="lg:inline-block " /><span className=''>Software Developer</span>
           </h1>
-          <p className="mb-8 leading-relaxed text-gray-700">
+          <p className="mb-8 leading-relaxed text-gray-700 md:text-base text-sm">
             With a background in both frontend and backend development, I specialize in building full-stack web applications that are robust, user-friendly, and efficient. 
             My expertise includes working with JavaScript, React, Node.js, and more. I love solving complex problems and delivering high-quality code that makes a difference.
           </p>
-          <div className="flex justify-center">
+          <div className="flex justify-center ">
             <a href="#projects">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 md:px-6 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 md:px-6 px-4 focus:outline-none hover:bg-indigo-600 rounded md:text-lg text-sm ">
                 View Projects
               </button>
             </a>
             <a href="#contact" className="ml-4">
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 md:px-6 px-4 focus:outline-none hover:bg-gray-200 rounded text-lg">
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 md:px-6 px-4 focus:outline-none hover:bg-gray-200 rounded md:text-lg text-sm">
                 Contact Me
               </button>
             </a>
