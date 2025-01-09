c044d1be16e7d99517e2b89e22560a4a1261711d
unawarexi <unawarexi@gmail.com>
2025-01-09 16:24:25 +0100

    UPDATED

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Header from "./components/header/Header";
 import About from "./components/about/About";
-import Experience from "./components/experience/experience";
+import Experience from "./components/experience/Experience";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
 
