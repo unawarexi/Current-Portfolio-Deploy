54c9cf694d701b18e2a2849b1db031d5ef0f0ff0
unawarexi <unawarexi@gmail.com>
2025-04-06 04:28:04 +0100

    updated the responsiveness

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -32,10 +32,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        import.meta.env.VITE_EMAIL_SERVICE_ID, 
-        import.meta.env.VITE_EMAIL_TEMPLATE_ID, 
+        import.meta.env.VITE_EMAIL_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID,
         form.current,
-        import.meta.env.VITE_EMAIL_PUBLIC_KEY 
+        import.meta.env.VITE_EMAIL_PUBLIC_KEY
       )
       .then(
         (result) => {
@@ -66,10 +66,18 @@ const Contact = () => {
         </div>
 
         {/* Contact form container */}
-        <div className="relative z-10 w-full px-5 py-16 mx-auto md:px-8 md:max-w-7xl">
-          <div className="grid gap-8 md:grid-cols-2 items-start">
+        <div className="relative z-10 w-full  md:py-16 py-10 mx-auto md:px-8 md:max-w-7xl ">
+          <div
+            className={`${
+              isMobile ? "flex flex-wrap" : "grid md:grid-cols-2"
+            } gap-8 md:items-start items-center`}
+          >
             {/* Contact Form */}
-            <div className="bg-gray-50 dark:bg-black/70 bg-opacity-90 rounded-lg p-6 shadow-lg">
+            <div
+              className={`bg-gray-50 dark:bg-black/80 bg-opacity-90 rounded-lg p-6 shadow-lg ${
+                isMobile ? "w-[90%] mx-5" : ""
+              }`}
+            >
               <h2 className="text-lg font-bold text-indigo-900 mb-4 dark:text-white md:text-3xl">
                 Get in Touch
               </h2>
@@ -77,31 +85,39 @@ const Contact = () => {
                 Feel free to reach out, and get a response as soon as possible.
               </p>
 
-              <form className="space-y-4 h-auto" ref={form} onSubmit={sendEmail}>
+              <form
+                className="space-y-4 h-auto"
+                ref={form}
+                onSubmit={sendEmail}
+              >
                 <input
                   type="text"
                   name="name"
                   placeholder="Name"
                   required
-                  className="w-full p-3 rounded-lg bg-white border text-xs border-gray-200 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 outline-none dark:bg-gray-800 dark:border-gray-700 dark:text-white md:text-sm"
+                  className="w-full p-3 rounded-lg bg-white border text-xs border-gray-200 focus:border-indigo-500 focus:ring-1
+                   focus:ring-indigo-500 outline-none dark:bg-gray-800 dark:border-gray-700 dark:text-white md:text-sm"
                 />
                 <input
                   type="email"
                   name="email"
                   placeholder="E-mail"
                   required
-                  className="w-full p-3 rounded-lg bg-white border text-xs border-gray-200 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 outline-none dark:bg-gray-800 dark:border-gray-700 dark:text-white md:text-sm"
+                  className="w-full p-3 rounded-lg bg-white border text-xs border-gray-200 focus:border-indigo-500 focus:ring-1
+                   focus:ring-indigo-500 outline-none dark:bg-gray-800 dark:border-gray-700 dark:text-white md:text-sm"
                 />
                 <textarea
                   name="message"
                   placeholder="Message"
                   rows={4}
                   required
-                  className="w-full p-3 rounded-lg bg-white border text-xs border-gray-200 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 outline-none resize-none dark:bg-gray-800 dark:border-gray-700 dark:text-white md:text-sm"
+                  className="w-full p-3 rounded-lg bg-white border text-xs border-gray-200 focus:border-indigo-500 focus:ring-1
+                   focus:ring-indigo-500 outline-none resize-none dark:bg-gray-800 dark:border-gray-700 dark:text-white md:text-sm"
                 ></textarea>
                 <button
                   type="submit"
-                  className="w-full text-xs bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-3 px-6 rounded-lg transition-colors dark:bg-indigo-600 dark:hover:bg-indigo-700 md:text-sm"
+                  className="w-full text-xs bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-3 px-6 rounded-lg transition-colors
+                   dark:bg-indigo-600 dark:hover:bg-indigo-700 md:text-sm"
                 >
                   Send Message
                 </button>
@@ -109,8 +125,12 @@ const Contact = () => {
             </div>
 
             {/* Contact Information and Hours of Operation */}
-            <div className="space-y-8">
-              <div className="bg-white dark:bg-black/70 bg-opacity-90 rounded-lg p-6 shadow-lg">
+            <div
+              className={`md:space-y-8 space-y-4 ${
+                isMobile ? "w-[90%] mx-5 " : ""
+              }`}
+            >
+              <div className="bg-white dark:bg-black/70 bg-opacity-90 rounded-lg p-6 shadow-lg items-center justify-evenly">
                 <h3 className="text-md font-semibold text-indigo-900 mb-6 dark:text-white md:text-xl">
                   Contact Information
                 </h3>
@@ -127,7 +147,9 @@ const Contact = () => {
                       <h4 className="font-semibold text-gray-900 text-xs mb-1 dark:text-gray-200 md:text-sm">
                         Our Location
                       </h4>
-                      <p className="text-gray-600 text-xs dark:text-gray-400 md:text-sm">Nigeria</p>
+                      <p className="text-gray-600 text-xs dark:text-gray-400 md:text-sm">
+                        Nigeria
+                      </p>
                       <p className="text-gray-600 text-xs dark:text-gray-400 md:text-sm">
                         Benin City, Edo. - Lagos
                       </p>
@@ -140,7 +162,10 @@ const Contact = () => {
                       className="w-5 h-5 fill-indigo-900"
                       viewBox="0 0 513.64 513.64"
                     >
-                      <path d="M499.66 376.96l-71.68-71.68c-25.6-25.6-69.12-15.359-79.36 17.92-7.68 23.041-33.28 35.841-56.32 30.72-51.2-12.8-120.32-79.36-133.12-133.12-7.68-23.041 7.68-48.641 30.72-56.32 33.28-10.24 43.52-53.76 17.92-79.36l-71.68-71.68c-20.48-17.92-51.2-17.92-69.12 0L18.38 62.08c-48.64 51.2 5.12 186.88 125.44 307.2s256 176.641 307.2 125.44l48.64-48.64c17.921-20.48 17.921-51.2 0-69.12z" />
+                      <path
+                        d="M499.66 376.96l-71.68-71.68c-25.6-25.6-69.12-15.359-79.36 17.92-7.68 23.041-33.28 35.841-56.32 30.72-51.2-12.8-120.32-79.36-133.12-133.12-7.68-23.041
+                       7.68-48.641 30.72-56.32 33.28-10.24 43.52-53.76 17.92-79.36l-71.68-71.68c-20.48-17.92-51.2-17.92-69.12 0L18.38 62.08c-48.64 51.2 5.12 186.88 125.44 307.2s256 176.641 307.2 125.44l48.64-48.64c17.921-20.48 17.921-51.2 0-69.12z"
+                      />
                     </svg>
                     <div>
                       <h4 className="font-semibold text-gray-900 text-xs mb-1 dark:text-gray-200 md:text-sm">
@@ -158,7 +183,11 @@ const Contact = () => {
                       className="w-5 h-5 fill-indigo-900"
                       viewBox="0 0 512 512"
                     >
-                      <path d="M298.789 313.693c-12.738 8.492-27.534 12.981-42.789 12.981-15.254 0-30.05-4.489-42.788-12.981L3.409 173.82A76.269 76.269 0 0 1 0 171.403V400.6c0 26.278 21.325 47.133 47.133 47.133h417.733c26.278 0 47.133-21.325 47.133-47.133V171.402a75.21 75.21 0 0 1-3.416 2.422z" />
+                      <path
+                        d="M298.789 313.693c-12.738 8.492-27.534 12.981-42.789 12.981-15.254 0-30.05-4.489-42.788-12.981L3.409 173.82A76.269 
+                      76.269 0 0 1 0 171.403V400.6c0 26.278 21.325 47.133 47.133 47.133h417.733c26.278 0 47.133-21.325 47.133-47.133V171.402a75.21
+                       75.21 0 0 1-3.416 2.422z"
+                      />
                     </svg>
                     <div>
                       <h4 className="font-semibold text-gray-900 text-xs mb-1 dark:text-gray-200 md:text-sm">
@@ -167,7 +196,7 @@ const Contact = () => {
                       <a
                         href="mailto:opensource.chukwuweike.dev@gmail.com"
                         target="_blank"
-                        className="text-gray-600 text-xs font-bold dark:text-gray-400 md:text-sm"
+                        className="text-gray-600 text-[11px] font-bold dark:text-gray-400 md:text-sm"
                       >
                         opensource.chukwuweike.dev@gmail.com
                       </a>
