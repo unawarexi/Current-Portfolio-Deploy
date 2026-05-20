66f0a5cee7ced17ffdd060758ed1a4543479983f
Portfolio Deploy <portfolio@deploy.local>
2026-05-20 17:38:04 +0100

    updated

--- Frontend-portfolio/vite.config.js
+++ Frontend-portfolio/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
       '@assets':      fileURLToPath(new URL('./src/assets', import.meta.url)),
       '@app':         fileURLToPath(new URL('./src/app', import.meta.url)),
     },
+    
   },
 });
 
