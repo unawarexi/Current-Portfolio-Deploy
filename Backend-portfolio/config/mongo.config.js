6548af9fb124e0c62751cda379e14c7904c18a91
Portfolio Deploy <portfolio@deploy.local>
2026-06-22 14:10:45 +0100

    updated

--- Backend-portfolio/config/mongo.config.js
+++ Backend-portfolio/config/mongo.config.js
@@ -4,7 +4,6 @@
 // ============================================================================
 
 import mongoose from "mongoose";
-import "dotenv/config.js";
 import { createLogger } from "../logs/logger.js";
 const log = createLogger("MongoDB");
 
